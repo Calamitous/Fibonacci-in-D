0bf065b85eb9d17bd0dfce86d18d61ee35d5aac1
Eric Budd <ebudd@zavenetworks.com>
2011-06-08 11:20:08 -0500

    Added contract checks to fibonacci function

--- fib.d
+++ fib.d
@@ -14,21 +14,21 @@ void main(string[] args) {
   writeln(fibonacci(number_of_elements));
 }
 
-int[] fibonacci(int elements, int[] result = []) {
-  if (elements == 0) {
-    return result;
+int[] fibonacci(int elements, int[] result = [])
+  in {
+    assert(elements >= 0);
+    assert(elements < 48);
   }
-  int new_value = result.length < 2 ? result.length : sum_tail(result);
-  return fibonacci(elements - 1, result ~ new_value);
-}
-
-int sum_tail(int[] from_array) {
-  return from_array[$ - 2] + from_array[$ - 1];
-}
 
 int[] fibonacci_iterative(int elements) {
   if (elements == 0) {
     return [];
+  body {
+    if (elements == 0) {
+      return result;
+    }
+    int new_value = result.length < 2 ? result.length : sum_tail(result);
+    return fibonacci(elements - 1, result ~ new_value);
   }
 
   int[] result;
@@ -43,5 +43,7 @@ int[] fibonacci_iterative(int elements) {
     result[i] = sum_tail(result);
   }
   return result;
+int sum_tail(int[] from_array) {
+  return from_array[$ - 2] + from_array[$ - 1];
 }
 
