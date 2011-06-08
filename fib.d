bd371cd7031fb6c18bbeb52cf749c533a2ac6591
Eric Budd <ebudd@zavenetworks.com>
2011-06-08 10:00:37 -0500

    Added command-line argument to allow selection of fib depth at runtime

--- fib.d
+++ fib.d
@@ -1,11 +1,17 @@
-import std.stdio;
+import std.stdio, std.conv;
 
-void main() {
+void main(string[] args) {
   assert(fibonacci(0) == []);
   assert(fibonacci(1) == [0]);
   assert(fibonacci(3) == [0, 1, 1]);
   assert(fibonacci(21) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]);
-  writeln(fibonacci(10));
+  int number_of_elements = 10;
+
+  if (args.length >= 1) {
+    number_of_elements = to!int(args[1]);
+  }
+
+  writeln(fibonacci(number_of_elements));
 }
 
 int[] fibonacci(int elements, int[] result = []) {
