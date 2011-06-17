1c825fbc17b95d92695c1da8acf51db86730412d
Eric Budd <ebudd@zavenetworks.com>
2011-06-17 11:15:03 -0500

    Updated unit tests to live in more rational location

--- fib.d
+++ fib.d
@@ -1,11 +1,6 @@
 import std.stdio, std.conv;
 
 void main(string[] args) {
-  assert(fibonacci(0) == []);
-  assert(fibonacci(1) == [0]);
-  assert(fibonacci(3) == [0, 1, 1]);
-  assert(fibonacci(21) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]);
-
   int number_of_elements = 10;
   if (args.length > 1) {
     number_of_elements = to!int(args[1]);
@@ -31,4 +26,14 @@ int[] fibonacci(int elements, int[] result = [])
 
 int sum_tail(int[] from_array) {
   return from_array[$ - 2] + from_array[$ - 1];
+}
+
+unittest {
+  assert(fibonacci(0)  == []);
+  assert(fibonacci(1)  == [0]);
+  assert(fibonacci(3)  == [0, 1, 1]);
+  assert(fibonacci(21) == [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]);
+
+  assert(sum_tail([4, 5, 6, 7]) == 13);
+  assert(sum_tail([1, 1])       == 2);
 }
